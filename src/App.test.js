fddbbac97adfab62b675d25a9691f4242347bc71
Jared Schuckman <jschuckm@iastate.edu>
2021-06-06 16:00:03 -0500

    updated test and env

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello World/i);
+  const linkElement = screen.getByText(/Jared Schuckman/i);
   expect(linkElement).toBeInTheDocument();
 });
