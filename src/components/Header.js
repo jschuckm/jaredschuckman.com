b8322f6d66e9d10edb2864bcf8849ecc597c3d5f
Jared Schuckman <jschuckm@iastate.edu>
2021-06-08 21:20:10 -0500

    fixed links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import './Header.css'
+import {Link} from "react-router-dom"
+import {useState} from "react"
 
 function Header() {
+
+  const [expand, setExpand] = useState(false);
+
   return (
     <div style={{width:"100%"}}>
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="lg" onToggle={()=>setExpand(!expand)} expanded={expand}>
         <div className="brand">
         <Navbar.Brand href="#home">Jared Schuckman</Navbar.Brand>
         </div>
@@ -13,10 +18,10 @@ function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
             <Nav>
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="projects">Projects</Nav.Link>
-                <Nav.Link href="about">About</Nav.Link>
-                <Nav.Link href="contact">Contact</Nav.Link>
+                <Nav.Link onClick={()=>{setExpand(false)}} as={Link} to="/">Home</Nav.Link>
+                <Nav.Link onClick={()=>{setExpand(false)}} as={Link} to="projects">Projects</Nav.Link>
+                <Nav.Link onClick={()=>{setExpand(false)}} as={Link} to="about">About</Nav.Link>
+                <Nav.Link onClick={()=>{setExpand(false)}} as={Link} to="contact">Contact</Nav.Link>
             </Nav>
         </Navbar.Collapse>
         </div>
